57c14f9adda25548ea2edfafd005b559a9064120
VetagroTI2 <ti2@veteagro.com.br>
2025-08-22 15:20:41 +0000

    Teste

--- app-estacio/src/app/index.tsx
+++ app-estacio/src/app/index.tsx
@@ -1,8 +1,18 @@
-import { View, Text } from "react-native"
-export default function Index() {
+import { View, Text, StyleSheet } from "react-native"
+import { Link } from "expo-router"
+export default function HomeScream() {
     return (
-        <View>
-            <Text>Hello World</Text>
+        <View style={styles.container}>
+            <Text>Hello World!</Text>
+            <Link href={"/details"}>Ver detalhes</Link>
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
